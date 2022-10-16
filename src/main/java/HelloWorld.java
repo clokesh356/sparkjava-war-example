2768238844698d7e9766d621c7d89bd48c61d0f3
clokesh356 <clokeshreddy356@gmail.com>
2022-10-16 10:20:35 +0530

    updated java code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Lokesh Reddy");
+		get("/hello", (req, res) -> "Deploying to tomcat");
 	}
 }
